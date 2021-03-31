45a7b6c199525bbfc81c682e5730b24c56353f39
Joseph Abraham <ekurubal24@gmail.com>
2021-03-31 22:36:35 +0300

    Created new div, li- adding the created div to li

--- todo.js
+++ todo.js
@@ -1,1 +1,26 @@
-console.log("I am here")
+const newTask = document.querySelector('.task-input');
+const newTaskAddBtn = document.querySelector('.btn-task-add');
+const taskList = document.querySelector('.task-list');
+
+newTaskAddBtn.addEventListener('click', taskAdd);
+
+function taskAdd(e) {
+    e.preventDefault();
+    // div created
+    const taskDiv = document.createElement('div');
+    taskDiv.classList.add('task-item');
+
+    // li created
+
+    const taskLi = document.createElement('li');
+    taskLi.classList.add('task-define');
+    taskLi.innerText = newTask.value;
+
+    taskDiv.appendChild(taskLi);
+
+    //adding the created div to ul
+
+    taskList.appendChild(taskDiv);
+
+    //console.log('click me')
+}
